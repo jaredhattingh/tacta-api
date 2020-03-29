604ebc3cd2aee0afb26bc63244afed8af6c2f24f
Duane Bester <bester.duane@gmail.com>
2020-03-28 21:31:54 -0700

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
         NODE_ENV: "development",
         MONGODB_URL: "mongodb+srv://tacta-dev-db:tacta-dev-12345@dev-iet4h.gcp.mongodb.net/test?retryWrites=true&w=majority",
         JWT_KEY: "WinterIsComingGOT2019",
-        PORT=3000
+        PORT: 3000
       },
     }
   ]
